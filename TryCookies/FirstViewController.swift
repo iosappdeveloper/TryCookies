24f30ec96a39eb6577eb3569df4dd380f778025a
Ashok Matoria <ashok.matoria@sap.com>
2020-02-04 11:43:45 -0500

    create and consume cookies initial implementation

--- TryCookies/FirstViewController.swift
+++ TryCookies/FirstViewController.swift
@@ -7,14 +7,107 @@
 //
 
 import UIKit
+import WebKit
+import Commons
+import UICommons
 
 class FirstViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var launchButton: UIButton!
+    
+    @IBAction func tapLaunchButton(_ sender: Any) {
+        UserDefaults.standard.clearCookies()
+        
+        let createCookiesVC = CreateCookieHybridViewController()
+        navigationController?.pushViewController(createCookiesVC, animated: true)
     }
-
-
 }
 
+class CreateCookieHybridViewController: UIViewController {
+    var webView: WKWebView = {
+        let configuration = WKWebViewConfiguration()
+        configuration.websiteDataStore = .default()
+        let webView = WKWebView(frame: .zero, configuration: configuration)
+        return webView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupSubViews()
+        
+        webView.load(URLRequest(url: Environment.authWebPageUrl))
+    }
+    
+    func setupSubViews() {
+        Layout.add(webView, in: view)
+        webView.navigationDelegate = self
+    }
+}
+
+extension CreateCookieHybridViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("\(#function) >>> ", webView.url ?? "no url")
+        if webView.url?.path.contains(Environment.authSuccessInterceptorPath) ?? false {
+            webView.configuration.websiteDataStore.httpCookieStore.getAllCookies { (cookies) in
+                UserDefaults.standard.saveCookies(cookies)
+            }
+        }
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Swift.Error) {
+        UIUtils.showAlert(title: error.localizedDescription)
+    }
+}
+
+extension UserDefaults {
+    private var cookiesKey: String {
+        return "cookies"
+    }
+    
+    func saveCookies(_ cookies: [HTTPCookie]) {
+        var cookieProperties = [[HTTPCookiePropertyKey: Any]]()
+        cookies.forEach { cookieProperties.append($0.properties!) }
+        UserDefaults.standard.setValue(cookieProperties, forKey: cookiesKey)
+        let success = UserDefaults.standard.synchronize()
+        assert(success)
+    }
+    
+    func getCookies() -> [HTTPCookie] {
+        var savedCookies = [HTTPCookie]()
+        guard let cookiesValue = UserDefaults.standard.value(forKey: cookiesKey) else { return savedCookies }
+        guard let cookieProperties = cookiesValue as? [[HTTPCookiePropertyKey: Any]] else { return savedCookies }
+        cookieProperties.forEach {
+            if let cookie = HTTPCookie(properties: $0) {
+                savedCookies.append(cookie)
+            }
+        }
+        return savedCookies
+    }
+    
+    func clearCookies() {
+        setValue(nil, forKey: cookiesKey)
+        synchronize()
+    }
+}
+
+struct Environment {
+    static let host = "google.com"//"sc8lqmt01.plateau.com"
+    static let authWebPagePath = ""//"/learning/user/nativelogin.jsp"
+    static let authSuccessInterceptorPath = "/"//"/learning/user/personal/landOnPortalHome.do"
+    static let homeWebPagePath = "/mail"//"/learning/user/personal/landOnPortalHome.do"
+    
+    static let authWebPageUrl: URL = {
+        return absoluteUrl(Environment.authWebPagePath)
+    }()
+    
+    static let homeWebPageUrl: URL = {
+        return absoluteUrl(Environment.homeWebPagePath)
+    }()
+    
+    private static func absoluteUrl(_ path: String) -> URL {
+        var comp = URLComponents()
+        comp.scheme = "https"
+        comp.host = Environment.host
+        comp.path = path
+        return comp.url!
+    }
+}
--- TryCookies/SecondViewController.swift
+++ TryCookies/SecondViewController.swift
@@ -7,14 +7,80 @@
 //
 
 import UIKit
+import WebKit
+import UICommons
+import Commons
 
 class SecondViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var launchButton: UIButton!
+    
+    @IBAction func touchLaunchButton(_ sender: Any) {
+        let consumeCookieHybridVC = ConsumeCookieHybridViewController()
+        navigationController?.pushViewController(consumeCookieHybridVC, animated: true)
     }
-
-
 }
 
+class ConsumeCookieHybridViewController: UIViewController {
+    var webView: WKWebView = {
+        let configuration = WKWebViewConfiguration()
+        configuration.websiteDataStore = .nonPersistent()
+        let webView = WKWebView(frame: .zero, configuration: configuration)
+        return webView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupSubViews()
+        
+        load(url: Environment.homeWebPageUrl)
+        // Refresh button
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(refresh))
+    }
+    
+    func setupSubViews() {
+        Layout.add(webView, in: view)
+        webView.navigationDelegate = self
+    }
+}
+
+extension ConsumeCookieHybridViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print(webView.url ?? "no url")
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Swift.Error) {
+        UIUtils.showAlert(title: error.localizedDescription)
+    }
+}
+
+private extension ConsumeCookieHybridViewController {
+    func load(url: URL) {
+        copyCookies { [weak self] in
+            self?.webView.load(URLRequest(url: url))
+        }
+    }
+    
+    func copyCookies(_ completion: @escaping () -> Void) {
+        let cookies = UserDefaults.standard.getCookies()
+        let dispatchGroup = DispatchGroup()
+        
+        // Move cookies into `httpCookieStore` of this web view before loading the url
+        for cookie in cookies {
+            dispatchGroup.enter()
+            webView.configuration.websiteDataStore.httpCookieStore.setCookie(cookie) {
+                dispatchGroup.leave()
+            }
+        }
+        
+        dispatchGroup.notify(queue: .main) {
+            completion()
+        }
+    }
+    
+    @objc func refresh() {
+        copyCookies { [weak self] in
+            
+        }
+    }
+}
